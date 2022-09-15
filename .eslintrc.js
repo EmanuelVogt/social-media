36b6793802511ef62902edf5da5d508332673e5d
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-09-14 22:25:29 -0300

    chore: configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,13 +15,37 @@ module.exports = {
     project: './tsconfig.json'
   },
   plugins: [
-    'react'
+    'react',
+    'simple-import-sort'
   ],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'camelcase': 'error',
     'spaced-comment': 'error',
     'quotes': ['error', 'single'],
-    'no-duplicate-imports': 'error'
+    'no-duplicate-imports': 'error',
+    'indent': ['error', 2],
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        'groups': [
+          // Packages `react` related packages come first.
+          ['^react', '^@?\\w'],
+          // Internal packages.
+          ['^(@|components)(/.*|$)'],
+          // Side effect imports.
+          ['^\\u0000'],
+          // Parent imports. Put `..` last.
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Style imports.
+          ['^.+\\.?(css)$']
+        ]
+      }],
+    'space-before-blocks': ['error', { 'functions': 'always', 'keywords': 'always', 'classes': 'always' }],
+    'object-curly-spacing': ['error', 'always'],
+    '@typescript-eslint/consistent-type-definitions': 'off'
   }
 }
